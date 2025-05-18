67e0844ebaa3bae4f7f8a75d3775092d0ead2ee5
iamprashu <iamprashu.gs@gmail.com>
2025-05-18 21:42:09 +0530

    Chat List And Related Data

---
+++ src/components/ChatBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChatBox() {
+  return (
+    <div className='bg-gray-800 h-full w-full'>
+      
+    </div>
+  )
+}
---
+++ src/components/ChatList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Chat from './Chat'
+
+function ChatList() {
+  return (
+    <div className="w-full h-full/15 p-2 flex flex-col gap-2 h-full overflow-scroll no-scrollbar ">
+        <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/> <Chat/>
+
+    </div>
+  )
+}
+
+export default ChatList
