bac10cbcccb9432d658f6876394e663a1c79832f
iamprashu <iamprashu.gs@gmail.com>
2025-05-18 21:42:26 +0530

    Chats Demo

---
+++ src/components/Chat.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Chat() {
+  return (
+    <div className='bg-gray-950/40 w-full min-h-15 flex items-center p-2 rounded-2xl text-white gap-2'>
+        <div className='rounded-full'>
+            <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg" alt="" className='h-10 w-10 rounded-full' />
+        </div>
+        <div>Prashant Joshi</div>
+    </div>
+  )
+}
+
+export default Chat
