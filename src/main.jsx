792850a5b4031f8f83c015a3d5111fc95f68a8cf
iamprashu <iamprashu.gs@gmail.com>
2025-05-21 23:44:47 +0530

    Context Used for Ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import  UiContext  from './Contexts/UiContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <UiContext>
+        <App />
+    </UiContext>
+    
 )
