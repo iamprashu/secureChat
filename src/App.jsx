05c8bbbf979ba08c8a07f3a9d4c08fb703872818
iamprashu <iamprashu.gs@gmail.com>
2025-05-21 23:43:58 +0530

    App Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import LeftPanel from './components/LeftPanel'
 import OuterPanel from './components/OuterPanel'
 import AppHeader from './components/AppHeader'
 import ChatBox from './components/ChatBox'
+import MobileMenu from './components/MobileMenu'
+import { useContext, useState } from 'react'
+import { UiContext } from './Contexts/UiContext'
 
 function App() {
+  const {mobileMenu,setMobileMenu} = useContext(UiContext);
 
   return (
     <div className='bg-gray-800 h-screen flex flex-col overflow-hidden'>
       <AppHeader/>
       <div className='flex h-screen'>
           <OuterPanel/>
-        <LeftPanel/>
+          {mobileMenu && <LeftPanel/>}
+
         <ChatBox/>
       </div>
     </div>
