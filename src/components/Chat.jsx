b1c3c328015477a370f0595a611e42023e953c83
iamprashu <iamprashu.gs@gmail.com>
2025-05-22 23:24:42 +0530

    Chat COmponent

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 function Chat() {
   return (
-    <div className='bg-gray-950/40 w-full min-h-15 flex items-center p-2 rounded-2xl text-white gap-2'>
-        <div className='rounded-full'>
-            <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg" alt="" className='h-10 w-10 rounded-full' />
+    <div className='bg-gray-950/40 p-2 w-full min-h-20 flex items-center  justify-between rounded-2xl text-white gap-2'>
+        <div className='flex gap-2 justify-center items-center'>
+          <div className='rounded-full'>
+              <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg" alt="" className='h-19 w-19 rounded-full ' />
+          </div>
+          <div>Prashant Joshi</div>
         </div>
-        <div>Prashant Joshi</div>
+
+        <div className='flex min-h-full items-end justify-end'>
+          <h2>19:00</h2>
+        </div>
     </div>
   )
 }
