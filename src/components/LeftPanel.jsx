0674b9c54d7c13e264b5e2f5d55223e25dce5f3c
iamprashu <iamprashu.gs@gmail.com>
2025-05-22 23:26:10 +0530

    Panels Updated

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -4,7 +4,7 @@ import ChatList from './ChatList'
 
 function LeftPanel() {
   return (
-    <div className='bg-gray-800 h-screen/15  border-r-1 rounded-tl-xl'>
+    <div className='bg-gray-800 h-screen/15 min-w-90  border-r-1 rounded-tl-xl'>
         <LeftPanelHeader/>
         <ChatList/>
         
