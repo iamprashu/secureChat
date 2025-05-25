702b54a17433d2429162ec8d9fa8b2b67fe02232
iamprashu <iamprashu.gs@gmail.com>
2025-05-25 07:50:07 +0530

    Chat Box Component

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,12 +3,19 @@ import { useContext } from 'react';
 import { FaPen } from 'react-icons/fa'
 import { UiContext } from '../Contexts/UiContext'
 import FriendPanel from './FriendPanel';
+import { Route,Routes } from 'react-router-dom';
+import Setting from './Setting';
 
 export default function ChatBox() {
   const {mobileMenu,friendPanel} = useContext(UiContext);
   return (
-    <div className='bg-gray-800 h-full w-full text-gray-600'>
+    <div className='bg-gray-800 h-full w-full text-gray-600 relative'>
       {friendPanel && <FriendPanel/>}
+      <Routes>
+              <Route path='/setting' element={<Setting/>}>
+      
+              </Route>
+            </Routes>
      <div className='h-full w-full flex flex-col items-center justify-center gap-5'>
         <div className='text-6xl'>
            <FaPen size={100}/>
