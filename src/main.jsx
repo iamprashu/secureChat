008e51388bd604eab72e6ed72f950e728f6a6683
iamprashu <iamprashu.gs@gmail.com>
2025-05-25 07:49:04 +0530

    Main Component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import  UiContext  from './Contexts/UiContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <UiContext>
-        <App />
-    </UiContext>
+    <BrowserRouter>
+        <UiContext>
+            <App />
+        </UiContext>
+    </BrowserRouter>
+  
     
 )
