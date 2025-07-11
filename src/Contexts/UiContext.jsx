6e2deb5a66e9795ecd977a24341343bf7be3eee7
iamprashu <iamprashu.gs@gmail.com>
2025-07-11 23:20:36 +0530

    Updated Ui context and Router Module

--- src/Contexts/UiContext.jsx
+++ src/Contexts/UiContext.jsx
@@ -5,12 +5,15 @@ export const UiContext = createContext();
 export default function ProviderFunction({ children }) {
   const [mobileMenu, setMobileMenu] = useState(window.innerWidth <= 793);
   const [friendPanel, setFriendPanel] = useState(false);
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
 
   const values = {
     mobileMenu,
     setMobileMenu,
     friendPanel,
     setFriendPanel,
+    openMobileMenu,
+    setOpenMobileMenu,
   };
 
   return <UiContext.Provider value={values}>{children}</UiContext.Provider>;
